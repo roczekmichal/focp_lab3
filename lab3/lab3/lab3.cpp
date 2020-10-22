d3cdec05b8c584254cf35894bb14d71094e394c7
roczekmichal <mich.rocz@gmail.com>
2020-10-22 12:24:33 +0200

    'Struct'

---
+++ lab3/lab3/lab3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <array>
+
+
+//Define a struct
+struct point2D
+{
+	double x;
+	double y;
+
+};
+
+
+int main()
+{
+	//Declare a variable with struct
+	point2D mypoint;
+	//Initialize its members
+	mypoint.x = 3.3;
+	mypoint.y = 4;
+
+	//Print its content
+	std::cout<<"Value of x:" << mypoint.x << std::endl;
+	std::cout<<"Value of y:" << mypoint.y << std::endl;
+
+
+	return 0;
+}
+
