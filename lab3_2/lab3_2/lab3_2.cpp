bf04f927e1039bc7b437650d0dc752ef4a96511e
roczekmichal <mich.rocz@gmail.com>
2020-10-22 14:22:10 +0200

    'Vector_matrix'

---
+++ lab3_2/lab3_2/lab3_2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+
+struct matrix2D {
+    std::vector<std::vector<int>> contents;
+
+    void print() {
+        std::cout << "Contents of the matrix:" << std::endl; 
+
+        int rows = contents.size();
+        int cols = contents[0].size();
+
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                std::cout << contents[i][j] << " ";               
+            }
+            std::cout << std::endl;
+        }
+    }
+};
+
+
+int main()
+{
+    matrix2D a;
+    matrix2D b;
+
+    a.contents = { {1,2}, {4,5}, {7,8} };
+    b.contents = { {9,8,7}, {6,5,4}, {3,2,1} };
+
+    a.print();
+    b.print();
+
+    return 0;
+}
+
